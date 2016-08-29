c61f7a439f95f3b0461dd022e88c3f8f2b3db52f
a9b <a9b.word@gmail.com>
2016-08-29 18:30:29 +0900

    ページング追加

--- index.php
+++ index.php
@@ -3,8 +3,11 @@ require 'vendor/autoload.php';
 $dir = dirname(dirname(dirname(__FILE__))) .  "/lobona_camera/";
 define("DIR_PHOTO",$dir);
 
-$url = "http://lobona-photo.a9b.jp/";
+$url = "http://a9b.jp/lobona_camera/";
 define("IMAGE_HOSTNAME",$url);
+
+# 1pageでの表示数
+define("VIEW_LIMIT", 10);
 
 use Slim\Slim;
 //use Slim\Extras\Views\Twig as Twig;
@@ -56,12 +59,18 @@ $app->get('/', function () use ($app){
 });
 
 $app->get('/date/:date', function ($date) use ($app) {
-    $a_file_path = get_file_path($date);
-    $a_file_path = array_map(function($path){
+    $files = get_file_path($date);
+    //$a_file_path = array_map(function($path){
+    //  return str_replace(DIR_PHOTO,IMAGE_HOSTNAME,$path);
+    //},$a_file_path);
+    $files = array_map(function($path){
       return str_replace(DIR_PHOTO,IMAGE_HOSTNAME,$path);
-    },$a_file_path);
+    },$files);
 
-    sort($a_file_path);
+    sort($files);
+    $page = $app->request->get('page', 0);
+    $_a_file_path = array_chunk($files, VIEW_LIMIT, true);
+    $a_file_path = $_a_file_path[$page];
 
     $app->view->setData('date', $date);
     $app->view->setData('a_file_path', $a_file_path);
